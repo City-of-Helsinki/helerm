cabcf40abf539bab57251ec7f35dfb55a2403de4
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-10-17 11:42:06 +0300

    Allow setting page_size for pagination
    Closes #4

--- helerm/settings.py
+++ helerm/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
-    'PAGE_SIZE': 20
+    'DEFAULT_PAGINATION_CLASS': 'metarecord.pagination.MetaRecordPagination',
 }
 
 
