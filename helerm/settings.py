b5f61f560c9b92d9e4a0d0706ec464adf6de9783
Tuomas Haapala <tuomas.haapala@anders.fi>
2017-02-16 15:33:10 +0200

    Function POST/PUT tests

--- helerm/settings.py
+++ helerm/settings.py
@@ -177,6 +177,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'metarecord.pagination.MetaRecordPagination',
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 
--- metarecord/tests/conftest.py
+++ metarecord/tests/conftest.py
@@ -1,16 +1,23 @@
 import pytest
 
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
 from metarecord.models import Action, Attribute, AttributeValue, Function, Phase, Record
 
 
 @pytest.fixture
-def function():
-    return Function.objects.create(name='test function', function_id='00 00')
+def parent_function():
+    return Function.objects.create(name='test parent function', function_id='00')
 
 
 @pytest.fixture
-def second_function():
-    return Function.objects.create(name='second test function', function_id='00 01')
+def function(parent_function):
+    return Function.objects.create(name='test function', function_id='00 00', parent=parent_function)
+
+
+@pytest.fixture
+def second_function(parent_function):
+    return Function.objects.create(name='second test function', function_id='00 01', parent=parent_function)
 
 
 @pytest.fixture
@@ -61,3 +68,20 @@ def free_text_value_2(free_text_attribute):
 @pytest.fixture
 def template():
     return Function.objects.create(name='test template', is_template=True)
+
+
+@pytest.fixture
+def user():
+    return get_user_model().objects.create(username='test_user')
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def user_api_client(user):
+    api_client = APIClient()
+    api_client.force_authenticate(user)
+    return api_client
