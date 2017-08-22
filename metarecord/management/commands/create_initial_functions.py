63d00cced3e63fc41efd1e495268fc3a6284bc17
Tuomas Haapala <tuomas.haapala@anders.fi>
2017-08-22 23:10:54 +0300

    Add temporary management script for creating initial functions
    The command is only needed for legacy reasons and should be removed
    as soon as it isn't needed anymore.
    
    Refs #48

---
+++ metarecord/management/commands/create_initial_functions.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from metarecord.models import Classification, Function
+
+
+class Command(BaseCommand):
+    help = "Create initial functions (for backwards compatibility)."
+
+    def __init__(self):
+        super().__init__()
+
+    def handle(self, *args, **options):
+        print('Creating initial functions...')
+
+        for classification in Classification.objects.all():
+            obj, created = Function.objects.get_or_create(classification=classification)
+            if created:
+                print(classification)
+
+        print('Done.')
