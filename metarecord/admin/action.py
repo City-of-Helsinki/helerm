4e9e0fb4a17fea2471322be50eaf2ee2009dc0cc
Eetu Häivälä <eetu.haivala@anders.fi>
2018-09-11 12:19:46 +0300

    Improve action admin usability
    Separate the information in action admin list view to multiple
    columns. Add filtering by classification code.

--- metarecord/admin/action.py
+++ metarecord/admin/action.py
@@ -7,12 +7,27 @@ from metarecord.models.action import Action
 
 @admin.register(Action)
 class ActionAdmin(StructuralElementAdmin):
-    fields = ('name', 'phase', 'attributes')
-    ordering = ('phase__function__classification__code', 'index')
-    raw_id_fields = ('phase',)
-    readonly_fields = ('name',)
+    list_display = ('get_classification_code', 'get_function_name', 'get_phase_name', 'get_name')
+    list_filter = ('phase__function__classification__code',)
     search_fields = ('attributes',)
 
-    def name(self, obj):
+    fields = ('get_name', 'phase', 'attributes')
+    ordering = ('phase__function__classification__code', 'index')
+    raw_id_fields = ('phase',)
+    readonly_fields = ('get_name',)
+
+    def get_classification_code(self, obj):
+        return obj.phase.function.get_classification_code()
+    get_classification_code.short_description = _('code')
+
+    def get_function_name(self, obj):
+        return obj.phase.function.get_name()
+    get_function_name.short_description = _('function')
+
+    def get_phase_name(self, obj):
+        return obj.phase.get_name()
+    get_phase_name.short_description = _('phase')
+
+    def get_name(self, obj):
         return obj.get_name()
-    name.short_description = _('name')
+    get_name.short_description = _('name')
