660dfb53fd111c3c8da011e6438e44fd631180ba
Eetu Häivälä <eetu.haivala@anders.fi>
2019-05-03 09:00:28 +0300

    List bulk update permissions in user API permission list
    Add user bulk update permissions to user API endpoint permission list.
    These permissions are used by the client app to hide/show things to
    users depending what users have permission to do.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import permissions, serializers, viewsets
 
 from metarecord.models import Function
+from metarecord.models.bulk_update import BulkUpdate
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,9 +14,27 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('uuid', 'username', 'first_name', 'last_name', 'permissions')
 
-    def get_permissions(self, obj):
+    def _get_user_bulk_update_permissions(self, user):
+        content_type = ContentType.objects.get_for_model(BulkUpdate)
+        permissions = Permission.objects.filter(content_type=content_type)
+        return [
+            perm.codename
+            for perm in permissions
+            if user.has_perm('%s.%s' % (content_type.app_label, perm.codename))
+        ]
+
+    def _get_user_function_permissions(self, user):
         app_label = Function._meta.app_label
-        return [perm[0] for perm in Function._meta.permissions if obj.has_perm('%s.%s' % (app_label, perm[0]))]
+        return [
+            perm[0]
+            for perm in Function._meta.permissions
+            if user.has_perm('%s.%s' % (app_label, perm[0]))
+        ]
+
+    def get_permissions(self, obj):
+        function_permissions = self._get_user_function_permissions(obj)
+        bulk_update_permissions = self._get_user_bulk_update_permissions(obj)
+        return function_permissions + bulk_update_permissions
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
