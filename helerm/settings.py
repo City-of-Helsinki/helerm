bf0064ef6bd634725b89b797ad51fc94538bb651
Ville Koivunen <ville.koivunen@hel.fi>
2020-05-27 12:47:55 +0300

    Use correct INSTALLED_APPS for updated django-helusers

--- helerm/settings.py
+++ helerm/settings.py
@@ -28,15 +28,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'helusers.apps.HelusersConfig',
+    'helusers.apps.HelusersAdminConfig',
     'django.contrib.postgres',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'helusers',
     'rest_framework',
     'corsheaders',
     'allauth',
