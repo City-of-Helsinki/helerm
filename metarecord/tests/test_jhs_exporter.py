edaf4204688039249174391469e15dee220dadf6
Daniel Prange <daniel.prange@anders.com>
2023-03-03 09:39:26 +0200

    Split JHS exporter test XML to its own file

--- metarecord/tests/test_jhs_exporter.py
+++ metarecord/tests/test_jhs_exporter.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from unittest import mock
 
@@ -10,8 +11,18 @@ from metarecord.models import Function
 from metarecord.views.export import JHSExportViewSet
 
 
+@pytest.fixture
+def jhs_export_xml_template(current_directory):
+    filename = "jhs_export_template.xml"
+    f = open(os.path.join(current_directory, "test_data", filename), "rb")
+    yield f
+    f.close()
+
+
 @pytest.mark.django_db
-def test_exporter_xml_generation_is_successful(function, phase, action, record):
+def test_exporter_xml_generation_is_successful(
+    jhs_export_xml_template, function, phase, action, record
+):
     function.state = Function.APPROVED
     function.save(update_fields=("attributes", "state"))
 
@@ -21,48 +32,10 @@ def test_exporter_xml_generation_is_successful(function, phase, action, record):
         with mock.patch("uuid.uuid4", return_value=mock_uuid):
             xml = JHSExporter().create_xml().decode("utf-8")
 
-    assert (
-        xml
-        == """<?xml version="1.0" encoding="utf-8"?>
-<tos:Tos xmlns:tos="http://skeemat.jhs-suositukset.fi/tos/2015/01/15">
- <tos:TosTiedot tos:id="{id}">
-  <tos:Nimeke>
-   <tos:NimekeKielella tos:kieliKoodi="fi">
-    <tos:NimekeTeksti>Helsingin kaupungin Tiedonohjaussuunnitelma</tos:NimekeTeksti>
-   </tos:NimekeKielella>
-  </tos:Nimeke>
-  <tos:YhteyshenkiloNimi>Tiedonhallinta</tos:YhteyshenkiloNimi>
-  <tos:TosVersio>1</tos:TosVersio>
-  <tos:TilaKoodi>3</tos:TilaKoodi>
-  <tos:OrganisaatioNimi>Helsingin kaupunki</tos:OrganisaatioNimi>
-  <tos:LisatiedotTeksti>JHS 191 XML 2020-04-01 12:00EEST exported from undefined environment</tos:LisatiedotTeksti>
- </tos:TosTiedot>
- <tos:Luokka tos:id="{func_id}">
-  <tos:Luokitustunnus>00 00</tos:Luokitustunnus>
-  <tos:Nimeke>
-   <tos:NimekeKielella tos:kieliKoodi="fi">
-    <tos:NimekeTeksti>test classification</tos:NimekeTeksti>
-   </tos:NimekeKielella>
-  </tos:Nimeke>
-  <tos:KasittelyprosessiTiedot tos:id="{id}">
-   <tos:Kayttorajoitustiedot/>
-   <tos:Sailytysaikatiedot/>
-   <tos:Toimenpidetiedot tos:id="{phase_id}">
-    <tos:ToimenpideluokkaTarkenneTeksti>test phase</tos:ToimenpideluokkaTarkenneTeksti>
-    <tos:Toimenpidetiedot tos:id="{action_id}">
-     <tos:ToimenpideluokkaTarkenneTeksti>test action</tos:ToimenpideluokkaTarkenneTeksti>
-     <tos:Asiakirjatieto tos:id="{rec_id}">
-      <tos:Kayttorajoitustiedot/>
-      <tos:Sailytysaikatiedot/>
-      <tos:AsiakirjaluokkaTeksti>None</tos:AsiakirjaluokkaTeksti>
-      <tos:AsiakirjaluokkaTarkenneTeksti>test record</tos:AsiakirjaluokkaTarkenneTeksti>
-     </tos:Asiakirjatieto>
-    </tos:Toimenpidetiedot>
-   </tos:Toimenpidetiedot>
-  </tos:KasittelyprosessiTiedot>
- </tos:Luokka>
-</tos:Tos>
-""".format(
+    expected_xml = (
+        jhs_export_xml_template.read()
+        .decode("utf-8")
+        .format(
             id=mock_uuid,
             func_id=function.uuid,
             phase_id=phase.uuid,
@@ -70,6 +43,8 @@ def test_exporter_xml_generation_is_successful(function, phase, action, record):
             rec_id=record.uuid,
         )
     )
+
+    assert xml == expected_xml
 
 
 @pytest.mark.django_db
