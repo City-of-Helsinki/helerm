1be7b9907123d162da1ee30f8dd06c03d8409bdb
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-03-30 15:34:39 +0300

    Create a production ready container
    Run backend with uWSGI on production mode without development
    requirements.
    
    - Move dev-requirement* -> requirements-dev*
    - Add uwsgi to requirements
    - Add uWSGI configuration
    - Run uWSGI from docker-entrypoint.sh
    - Add staticbuilder and production stages to the Dockerfile
    - Add ELASTICSEARCH_ANALYZER_MODE setting mainly for avoiding
      an error when collectstatic failed due to elasticsearch
      connection error.
    - compose.yml use development image and make django depend
      on elasticsearch
    
    Refs TIED-73

--- search_indices/__init__.py
+++ search_indices/__init__.py
@@ -1,56 +1,5 @@
-from elasticsearch_dsl import analyzer, token_filter
+from django.conf import settings
 
-from search_indices.utils import create_elasticsearch_connection
+from search_indices.utils import get_finnish_analyzer
 
-
-def get_finnish_stop_filter() -> token_filter:
-    return token_filter("finnish_stop", type="stop", stopwords="_finnish_")
-
-
-def get_finnish_stem_filter() -> token_filter:
-    return token_filter("finnish_stemmer", type="stemmer", language="finnish")
-
-
-def get_edge_ngram_filter() -> token_filter:
-    return token_filter(
-        "custom_edge_ngram_filter",
-        type="edge_ngram",
-        min_gram=3,
-        max_gram=15,
-    )
-
-
-def get_raudikko_filter() -> token_filter:
-    return token_filter(
-        "raudikko",
-        type="raudikko",
-    )
-
-
-def get_finnish_analyzer() -> analyzer:
-    es_connection = create_elasticsearch_connection()
-    if "raudikko" in es_connection.cat.plugins():
-        return analyzer(
-            "finnish_analyzer",
-            tokenizer="finnish",
-            filter=[
-                "lowercase",
-                "asciifolding",
-                "unique",
-                get_edge_ngram_filter(),
-                get_raudikko_filter(),
-            ],
-        )
-    else:
-        return analyzer(
-            "finnish_analyzer",
-            tokenizer="standard",
-            filter=[
-                "lowercase",
-                "asciifolding",
-                "unique",
-                get_edge_ngram_filter(),
-                get_finnish_stop_filter(),
-                get_finnish_stem_filter(),
-            ],
-        )
+FINNISH_ANALYZER = get_finnish_analyzer(settings.ELASTICSEARCH_ANALYZER_MODE)
--- search_indices/utils.py
+++ search_indices/utils.py
@@ -1,7 +1,67 @@
 from django.conf import settings
-from elasticsearch_dsl import connections
+from elasticsearch_dsl import analyzer, connections, token_filter
 
 
 def create_elasticsearch_connection():
     es_host = settings.ELASTICSEARCH_DSL["default"]["hosts"]
     return connections.create_connection(hosts=[es_host])
+
+
+def get_finnish_stop_filter() -> token_filter:
+    return token_filter("finnish_stop", type="stop", stopwords="_finnish_")
+
+
+def get_finnish_stem_filter() -> token_filter:
+    return token_filter("finnish_stemmer", type="stemmer", language="finnish")
+
+
+def get_edge_ngram_filter() -> token_filter:
+    return token_filter(
+        "custom_edge_ngram_filter", type="edge_ngram", min_gram=3, max_gram=15
+    )
+
+
+def get_raudikko_filter() -> token_filter:
+    return token_filter("raudikko", type="raudikko")
+
+
+finnish_raudikko_analyzer = analyzer(
+    "finnish_analyzer",
+    tokenizer="finnish",
+    filter=[
+        "lowercase",
+        "asciifolding",
+        "unique",
+        get_edge_ngram_filter(),
+        get_raudikko_filter(),
+    ],
+)
+
+finnish_standard_analyzer = analyzer(
+    "finnish_analyzer",
+    tokenizer="standard",
+    filter=[
+        "lowercase",
+        "asciifolding",
+        "unique",
+        get_edge_ngram_filter(),
+        get_finnish_stop_filter(),
+        get_finnish_stem_filter(),
+    ],
+)
+
+
+def get_finnish_analyzer(mode: str) -> analyzer:
+    if mode == "probe":
+        es_connection = create_elasticsearch_connection()
+        es_plugins = es_connection.cat.plugins()
+        mode = "raudikko" if "raudikko" in es_plugins else "standard"
+
+    if mode == "raudikko":
+        return finnish_raudikko_analyzer
+    elif mode == "standard":
+        return finnish_standard_analyzer
+
+    raise ValueError(
+        "Invalid argument. Possible values are: probe, raudikko and standard."
+    )
