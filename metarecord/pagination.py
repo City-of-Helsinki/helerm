2cf29efb0c526fd622dc35bfbeb2fc1d81ecca3d
Tuomas Haapala <tuomas.haapala@anders.com>
2026-09-07 14:15:48 +0300

    chore: temporary commit to test review env

--- metarecord/pagination.py
+++ metarecord/pagination.py
@@ -4,8 +4,11 @@ from django_elasticsearch_dsl_drf.pagination import (
 from rest_framework.pagination import PageNumberPagination
 
 
+DEFAULT_PAGE_SIZE = 20
+
+
 class MetaRecordPagination(PageNumberPagination):
-    page_size = 20
+    page_size = DEFAULT_PAGE_SIZE
     page_size_query_param = "page_size"
     max_page_size = 10000
 
